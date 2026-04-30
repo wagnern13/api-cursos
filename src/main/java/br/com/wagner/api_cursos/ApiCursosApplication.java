78b2c7a327c635ff2e1c9c1da4d353a868d2af77
wagnern13 <wagnern@hotmail.com.br>
2026-04-29 21:53:32 -0300

    Criação da estrutura do projeto

---
+++ src/main/java/br/com/wagner/api_cursos/ApiCursosApplication.java
@@ -0,0 +1,13 @@
+package br.com.wagner.api_cursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCursosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wagner/api_cursos/ApiCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wagner.api_cursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
